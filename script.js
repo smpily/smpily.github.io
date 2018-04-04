7b03cf5de83c90a50d0cf2d67d503fdb4805b1ce
smpily <37946625+smpily@users.noreply.github.com>
2018-04-03 20:33:54 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function sayHello (){
+
+	console.log("hello");
+}
+
+var sayBye = function (){
+
+	console.log("bye");
+}
+
+function sing(tone){
+
+	console.log(tone);
+}
+
+function multiply (a, b){
+	return a*b;
+}
+
